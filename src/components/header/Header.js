0c54ca01f41834b6c5eb607193fafaabd5fab26a
saurabh <sauravparmar878@gmail.com>
2024-04-20 11:24:02 +0530

    new changes made
    new changes made

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header_icon" src="https://images.squarespace-cdn.com/content/v1/60fdd46df4114c757bab2d83/1627285076626-XK9GG7JKEZ8E3F8FXGY0/Flicks-Logo-Red-1000px-v1.png" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
